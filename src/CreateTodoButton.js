574e96f5a1d4f3ea46dc15a4614942718ad8d34f
Donovan Rojas <rojasdrm03@gmail.com>
2023-06-16 14:14:55 -0500

    onClick in React

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -2,7 +2,8 @@ import './CreateTodoButton.css'
 
 function CreateTodoButton () {
     return(
-        <button type="button">
+        <button type="button" 
+            onClick={() => console.log('le diste click')}>
             <svg width="40" height="40" viewBox="0 0 56 56" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M28 3V53" stroke="#8A8A8A" stroke-width="5" stroke-linecap="round"/>
                 <path d="M3 28L53 28" stroke="#8A8A8A" stroke-width="5" stroke-linecap="round"/>
