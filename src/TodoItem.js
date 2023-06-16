bd93df397aad9f3f8b10737fde888ce9b8a20cf4
Donovan Rojas <rojasdrm03@gmail.com>
2023-06-16 13:19:25 -0500

    css styles button and search

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,10 +3,12 @@ import './TodoItem.css'
 function TodoItem (props) {
     return(
       <li className="TodoItem">
-        <input type="checkbox" className={`Icon Icon-check ${props.completed && "Icon-check--active"}`}/>
-        <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>
+        <label>
+          <input type="checkbox" className={`Icon Icon-check ${props.completed && "Icon-check--active"}`}/>
+          <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>
           {props.text}
-        </p>
+          </p>
+        </label>
         <span className="Icon Icon-delete">
           <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M12 12L2 2" stroke="#8A8A8A" stroke-width="3" stroke-linecap="round"/>
