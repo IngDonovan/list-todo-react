2061c405cd32f563ae8595f84d9f8c418ba2a0c8
Donovan Rojas <rojasdrm03@gmail.com>
2023-06-17 14:56:44 -0500

    fix warning

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ function App() {
   const completeTodo = (text) => {
     const newTodos = [...todos];
     const todoIndex = newTodos.findIndex(
-      (todo) => todo.text == text
+      (todo) => todo.text === text
     );
     // newTodos[todoIndex].completed = true;
     newTodos[todoIndex].completed = !newTodos[todoIndex].completed;
@@ -46,7 +46,7 @@ function App() {
   const deleteTodo = (text) => {
     const newTodos = [...todos];
     const todoIndex = newTodos.findIndex(
-      (todo) => todo.text == text
+      (todo) => todo.text === text
     );
     newTodos.splice(todoIndex, 1);
     setTodos(newTodos);
