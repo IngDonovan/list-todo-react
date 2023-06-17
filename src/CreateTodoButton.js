d8b020e2585fcb5ddc8dd5eefd0d0fbb22fcd8cc
Donovan Rojas <rojasdrm03@gmail.com>
2023-06-17 09:34:28 -0500

    Fix code

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -7,8 +7,8 @@ function CreateTodoButton () {
                 console.log('Vas a agregar una tarea')
                 }}>
             <svg width="40" height="40" viewBox="0 0 56 56" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M28 3V53" stroke="#8A8A8A" stroke-width="5" stroke-linecap="round"/>
-                <path d="M3 28L53 28" stroke="#8A8A8A" stroke-width="5" stroke-linecap="round"/>
+                <path d="M28 3V53" stroke="#8A8A8A" strokeWidth="5" strokeLinecap="round"/>
+                <path d="M3 28L53 28" stroke="#8A8A8A" strokeWidth="5" strokeLinecap="round"/>
             </svg>
         </button>
     );
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -5,15 +5,19 @@ function TodoItem (props) {
     return(
       <li className="TodoItem">
         <label>
-          <input type="checkbox" checked={props.completed} className={`Icon Icon-check ${props.completed && "Icon-check--active"}`}/>
-          <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>
-          {props.text}
+          <input 
+            type="checkbox" 
+            checked={props.completed} 
+            className={`Icon Icon-check ${props.completed ? 'Icon-check--active' : ''}`}/>
+          <p 
+            className={`TodoItem-p ${props.completed ? 'TodoItem-p--complete' : ''}`}>
+            {props.text}
           </p>
         </label>
         <span className="Icon Icon-delete">
           <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M12 12L2 2" stroke="#8A8A8A" stroke-width="3" stroke-linecap="round"/>
-            <path d="M12 2L2 12" stroke="#8A8A8A" stroke-width="3" stroke-linecap="round"/>
+            <path d="M12 12L2 2" stroke="#8A8A8A" strokeWidth="3" strokeLinecap="round"/>
+            <path d="M12 2L2 12" stroke="#8A8A8A" strokeWidth="3" strokeLinecap="round"/>
           </svg>
         </span>
       </li>
