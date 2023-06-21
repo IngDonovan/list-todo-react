be6a81687dbb8f78e182dca6d0126678800dce5b
Donovan Rojas <rojasdrm03@gmail.com>
2023-06-21 12:32:17 -0500

    change styles position and logic Counter

--- src/TodoCounter/index.js
+++ src/TodoCounter/index.js
@@ -1,23 +1,30 @@
 import './TodoCounter.css';
 
 function TodoCounter({ total, completed }) {
-  if (total === 0) {
-    return (
-      <h2>
-        Escribe tu primera tarea
-      </h2>
-    );
-  } else if (total === completed) {
-    return (
-      <h2 className='textCounter'>Felicidades, completaste todas las tareas!!!</h2>
-    );
-  } else {
-    return (
-      <h2>
-        Has completado {completed} de {total} tareas
-      </h2>
-    );
-  }
+  total === completed ? 
+  <h2 className='textCounter'>Felicidades, completaste todas las tareas!!!</h2> 
+  : 
+  <h2>
+  Has completado {completed} de {total} tareas
+  </h2>;
+
+  // if (total === 0) {
+  //   return (
+  //     <h2>
+  //       Escribe tu primera tarea
+  //     </h2>
+  //   );
+  // } else if (total === completed) {
+  //   return (
+  //     <h2 className='textCounter'>Felicidades, completaste todas las tareas!!!</h2>
+  //   );
+  // } else {
+  //   return (
+  //     <h2>
+  //       Has completado {completed} de {total} tareas
+  //     </h2>
+  //   );
+  // }
 }
 
   export { TodoCounter };//buena practica, evita que me pueda equivocar -exports nombrados
