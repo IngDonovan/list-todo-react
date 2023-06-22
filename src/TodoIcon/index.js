b6912569a6200e69df4a39b9f434bc03d5bd1324
Donovan Rojas <rojasdrm03@gmail.com>
2023-06-22 10:59:42 -0500

    little fix

--- src/TodoIcon/index.js
+++ src/TodoIcon/index.js
@@ -1,12 +1,11 @@
 import { ReactComponent as DeleteSVG } from './quit.svg';
 import { BsCheckCircleFill } from 'react-icons/bs';
-import { BsClipboardCheckFill } from 'react-icons/bs';
+
 import './TodoIcon.css';
 
 
 const iconTypes = {
     "check": (color) => <BsCheckCircleFill className='Icon-svg' fill={color}/>,
-    // "unCheck": (color) => <BsClipboardCheckFill className='Icon-svg' fill={color}/>,
     "delete": (color) => <DeleteSVG className='Icon-svg' fill={color}/>,
 };
 
